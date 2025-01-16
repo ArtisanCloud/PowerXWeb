61582a273bd4c0a5ecf6d9e55b0c4fccdb776263
Matrix-X <hbj418@gmail.com>
2025-01-16 13:38:58 +0800

    feat(tempalte)

---
+++ templates/PowerXCompoent/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import styles from './index.module.less';
+</script>
+
+<template>
+  <div :class="styles.container">
+    <!-- Replace with your component template -->
+  </div>
+</template>
